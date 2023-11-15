bae96286a8764ad2d969552959519f0489070fe3
RabihSassouh <rabih.sasouh@gmail.com>
2023-11-15 15:57:50 +0200

    added the funtion remove_driver that allows the user to remove a driver from the list of drivers.

--- Forth_assignment/transport.py
+++ Forth_assignment/transport.py
@@ -55,6 +55,15 @@ def remove_city():
         else:
             routes[driver].remove(delete_city)
         print(routes)
+#
+def remove_driver():
+    print("Please enter the name of the driver who you want to remove from the list of drivers.")
+    delete_driver=input("")
+    if delete_driver not in drivers:
+        print("The name you entered is not a driver in our company.")
+    else:
+        drivers.remove(delete_driver)
+    print(drivers)
 #the function that will run the main page.
 #
 def main():
@@ -77,8 +86,8 @@ def main():
             add_route()
         elif user_input==4:
             remove_city()
-        # elif user_input==5:
-        #     remove_driver()
+        elif user_input==5:
+            remove_driver()
         # elif user_input==6:
         #     check_deliver()
         elif user_input==7:
