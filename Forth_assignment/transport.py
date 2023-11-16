dbb689ad4736431a263362f38e43f27f1c287b20
RabihSassouh <rabih.sasouh@gmail.com>
2023-11-16 17:20:36 +0200

    changed the list of drivers to a list in a dic and fixed bugs

--- Forth_assignment/transport.py
+++ Forth_assignment/transport.py
@@ -1,12 +1,11 @@
 cities=["beirut","saida","batroun","zgharta","aley","zahle","sour","tripoli","akkar"]
-drivers=["alex","rawad","jhony","mohamad"]
-routes={
-    drivers[0] : ['beirut','saida','sour'] ,
-    'rawad' : ['beirut','aley','zahle'],
-    'jhony' : ['beirut','batroun','zgharta'],
-    'mohamad' : ['zgharta','tripoli','akkar']
-}
-print(routes)
+# drivers=["alex","rawad","jhony","mohamad"]
+drivers=[{'name':'alex','route':['beirut','saida','sour']},
+{'name': 'rawad','route':['beirut','aley','zahle']},
+{'name': 'jhony', 'route': ['beirut','batroun','zgharta']},
+{'name':'mohamad', 'route' : ['zgharta','tripoli','akkar']}]
+
+print(drivers)
 #the function that will add a city to the list of cities.
 #O(len(cities))
 def add_city():
@@ -22,48 +21,56 @@ def add_city():
 def add_driver():
     print("Please enter the first and last name of the driver you want to add: ")
     driver=input("")
-    if driver not in drivers:
-        drivers.append(driver)
-    else:
-        print("The driver you entered is already a part of our community!")
-    print(drivers)
+    for i in range (len(drivers)):
+        if drivers[i]['name']==driver:
+            print("The driver you entered is already a part of our community!")
+            return
+        else:
+            dic={'name':driver, 'route':[]}
+            drivers.append(dic)
+            print(drivers)
+            return 
+    
 
 #
-def add_route():
-    print("Please enter the driver that you want to add a city to his route: ")
-    driver=input("")
-    if driver not in drivers:
-        print("The driver's name you enter is not available in our driver's list.")
-    else:
-        print("Please enter the name of the city that you want to add to this driver's route: ")
-        new_city=input("")
-        if new_city not in routes.driver():
-            routes['driver'].append(new_city)
-    print(routes)
+# def add_route():
+#     print("Please enter the driver that you want to add a city to his route: ")
+#     driver=input("")
+#     if driver not in drivers:
+#         print("The driver's name you enter is not available in our driver's list.")
+#     else:
+#         print("Please enter the name of the city that you want to add to this driver's route: ")
+#         new_city=input("")
+#         if new_city not in routes[driver]:
+#             print("where ")
+#             routes[driver].append(new_city)
+#         else:
+#             print("the name of the city that you entered is already on this driver's route.")
+#     print(routes)
     
-#
-def remove_city():
-    print("Please enter the driver that you want to remove a city from his route: ")
-    driver=input("")
-    if driver not in drivers:
-        print("The driver's name you enter is not available in our driver's list.")
-    else:
-        print("Please enter the name of the city that you want to remove from",driver,"route.")
-        delete_city=input("")       
-        if delete_city not in routes[driver]:
-            print("The city you entered is already not in the route of ",driver)
-        else:
-            routes[driver].remove(delete_city)
-        print(routes)
-#
-def remove_driver():
-    print("Please enter the name of the driver who you want to remove from the list of drivers.")
-    delete_driver=input("")
-    if delete_driver not in drivers:
-        print("The name you entered is not a driver in our company.")
-    else:
-        drivers.remove(delete_driver)
-    print(drivers)
+# #
+# def remove_city():
+#     print("Please enter the driver that you want to remove a city from his route: ")
+#     driver=input("")
+#     if driver not in drivers:
+#         print("The driver's name you enter is not available in our driver's list.")
+#     else:
+#         print("Please enter the name of the city that you want to remove from",driver,"route.")
+#         delete_city=input("")       
+#         if delete_city not in routes[driver]:
+#             print("The city you entered is already not in the route of ",driver)
+#         else:
+#             routes[driver].remove(delete_city)
+#         print(routes)
+# #
+# def remove_driver():
+#     print("Please enter the name of the driver who you want to remove from the list of drivers.")
+#     delete_driver=input("")
+#     if delete_driver not in drivers:
+#         print("The name you entered is not a driver in our company.")
+#     else:
+#         drivers.remove(delete_driver)
+#     print(drivers)
 #the function that will run the main page.
 #
 def main():
@@ -82,12 +89,12 @@ def main():
             add_city()
         elif user_input==2:
             add_driver()
-        elif user_input==3:
-            add_route()
-        elif user_input==4:
-            remove_city()
-        elif user_input==5:
-            remove_driver()
+        # elif user_input==3:
+        #     add_route()
+        # elif user_input==4:
+        #     remove_city()
+        # elif user_input==5:
+        #     remove_driver()
         # elif user_input==6:
         #     check_deliver()
         elif user_input==7:
