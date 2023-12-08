4e224be12d8bba4ee4304c18c452a7e80b2495a2
RabihSassouh <rabih.sasouh@gmail.com>
2023-12-08 11:26:29 +0200

    created the function that checks if the input is palindrom

--- sixth_assignment/StacksAndQueues.py
+++ sixth_assignment/StacksAndQueues.py
@@ -2,8 +2,11 @@ class stack:
     def __init__(self):
         self.items=[]
     def IsEmpty(self):
-        if self.items==0:
-            return None
+        # return self.items==[]
+        if len(self.items)==0:
+            return True
+        else:
+            return False
     def push(self,value):
         self.items.append(value)
     def pop(self):
@@ -11,4 +14,20 @@ class stack:
             return None
         else:
             return self.items.pop()
-        
+s=stack()
+print("Please enter a word to check if it is a palindrom or not!")
+x=input("")
+print(x)
+reversed_input=[]
+list_input=[]
+for i in x:
+    s.push(i)
+    list_input.append(i)
+#creating the reversed of the input
+while s.IsEmpty() != True:
+    # s.pop()
+    reversed_input = reversed_input + [s.pop()]
+if reversed_input== list_input:
+    print("Waw,",x, "is a palindrom!")
+else:
+    print(x, "is not a palindrom...")
